6198a40e25123f9a882f497a90b186e7651362c8
Rabby-Cyber <ariyanrabby94@gmail.com>
2021-09-02 02:30:17 +0600

    book archive

---
+++ js/script.js
@@ -0,0 +1,37 @@
+const searchBooks = () => {
+  const searchField = document.getElementById("search-field");
+  const searchText = searchField.value;
+  console.log(searchText);
+
+  // clear search
+
+  searchField.value = "";
+
+  const url = `http://openlibrary.org/search.json?q=${searchText}`;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => displaySearchResults(data.docs));
+};
+
+const displaySearchResults = (books) => {
+  //   console.log(books);
+  const searchResults = document.getElementById("search-results");
+  books.forEach((book) => {
+    const div = document.createElement("div");
+    div.classList.add("col");
+    div.innerHTML = `
+    <div class="card h-100">
+      
+          <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top">
+          <div class="card-body">
+            <h1 class="card-title"> ${book.title} </h1>
+           <p> <strong> Author: </strong> ${book.author_name} </p>
+           <p> <strong> First Publish: </strong> ${book.first_publish_year} </p>
+           <p> <strong> Publisher: </strong> ${book.publisher} </p>
+          </div>
+        </div>
+    `;
+    searchResults.appendChild(div);
+  });
+};
