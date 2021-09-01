50457a3b3e91f82392c0ab3d2145862f148f1ec4
Rabby-Cyber <ariyanrabby94@gmail.com>
2021-09-02 04:01:36 +0600

    error handling

--- js/script.js
+++ js/script.js
@@ -5,31 +5,42 @@ const searchBooks = () => {
   // clear search
 
   searchField.value = "";
+  if (searchText === "") {
+    alert("No data entry 😧 Please Input Something 😊");
+  } else {
+    const url = `https://openlibrary.org/search.json?q=${searchText}`;
 
-  const url = `http://openlibrary.org/search.json?q=${searchText}`;
-
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => displaySearchResults(data.docs));
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => displaySearchResults(data.docs));
+  }
 };
 
 const displaySearchResults = (books) => {
   const searchResults = document.getElementById("search-results");
-  books.forEach((book) => {
-    const div = document.createElement("div");
-    div.classList.add("col");
-    div.innerHTML = `
-    <div class="card h-100">
-      
-        <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top">
-          <div class="card-body">
-            <h1 class="card-title"> ${book.title} </h1>
-           <p> <strong> Author: </strong> ${book.author_name} </p>
-           <p> <strong> First Publish: </strong> ${book.first_publish_year} </p>
-           <p> <strong> Publisher: </strong> ${book.publisher} </p>
-          </div>
-        </div>
-    `;
-    searchResults.appendChild(div);
-  });
+  searchResults.textContent = "";
+  const emptyMessage = document.getElementById("empty-message");
+
+  if (books.length == "") {
+    emptyMessage.innerText = "No Data Found 🥺 Please Input Valid Something 😊";
+  } else {
+    emptyMessage.textContent = "";
+    books.forEach((book) => {
+      const div = document.createElement("div");
+      div.classList.add("col");
+      div.innerHTML = `
+        <div class="card h-100">
+        
+            <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top">
+              <div class="card-body">
+                <h1 class="card-title"> ${book.title} </h1>
+               <p> <strong> Author: </strong> ${book.author_name} </p>
+               <p> <strong> First Publish: </strong> ${book.first_publish_year} </p>
+               <p> <strong> Publisher: </strong> ${book.publisher} </p>
+              </div>
+            </div>
+        `;
+      searchResults.appendChild(div);
+    });
+  }
 };
