2ba3b7200ffb6a54bb011f694e83820d01871863
Rabby-Cyber <ariyanrabby94@gmail.com>
2021-09-02 11:00:03 +0600

    bug fixed

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// find books
 const searchBooks = () => {
   const searchField = document.getElementById("search-field");
   const searchText = searchField.value;
@@ -12,29 +13,33 @@ const searchBooks = () => {
 
     fetch(url)
       .then((response) => response.json())
-      .then((data) => displaySearchResults(data.docs));
+      .then((data) => displaySearchResults(data));
   }
 };
 
+// display the search results
 const displaySearchResults = (books) => {
   const searchResults = document.getElementById("search-results");
+  const resultCounter = document.getElementById("result-count");
+  const numOfDataFoud = books.numFound;
   searchResults.textContent = "";
   const emptyMessage = document.getElementById("empty-message");
 
-  if (books.length == "") {
+  if (books.docs.length == "") {
     emptyMessage.innerText = "No Data Found 🥺 Please Input Valid Something 😊";
+    resultCounter.innerText = 0;
   } else {
     emptyMessage.textContent = "";
-
-    books.forEach((book) => {
+    resultCounter.innerText = numOfDataFoud;
+    books.docs.forEach((book) => {
       const div = document.createElement("div");
       div.classList.add("col");
 
       div.innerHTML = `
         <div class="card h-100">
-            
+         
             <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top">
-        <div class="card-body">
+               <div class="card-body">
             <h1 class="card-title"> ${book.title} </h1>
                <p> <strong> Author: </strong> ${book.author_name}</p>
                <p> <strong> First Publish: </strong> ${book.first_publish_year} </p>
